2d0d4f6b5b5bd84cfe5e6fd4316822a667bf9868
mayao <mayao@hujiang.com>
2018-01-06 17:57:41 +0800

    frontend css

--- server.js
+++ server.js
@@ -20,7 +20,14 @@ let server = http
 
 function router(req, res, url) {
     let type = url.substr(url.lastIndexOf('.') + 1, url.length)
-    res.writeHead(200, { 'Content-Type': `text/${type}` })
+    let contentType = ''
+    if( type == 'json' || type == 'js' || type == 'css' || type == 'html' ) {
+        contentType = `text/${type}`
+    }
+    if( type == 'gif' || type == 'jpg' || type == 'jpeg' || type == 'png' ) {
+        contentType = `image/${type}`
+    }
+    res.writeHead(200, { 'Content-Type': contentType })
     fs.readFile(`${__dirname}/${url}`, (err, data) => {
         if (!err) {
             res.end(data)
